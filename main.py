3cd32359d9346b782001d2a5fdd3aac87c3ddc0d
neuroxofficial3-web <neuroxofficial3@gmail.com>
2026-01-30 07:26:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 class TestApp(App):
     def build(self):
-        return Label(text="System Working - Mufadul")
+        return Button(text="Success! Mufadul")
 
 if __name__ == "__main__":
     TestApp().run()
